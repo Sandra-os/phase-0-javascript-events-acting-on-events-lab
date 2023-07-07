7b20f75798d742790ae8738c67f37068380f2805
sandra ogbudinkpa <sandra.os325@gmail.com>
2023-07-07 19:37:13 -0400

    moves object left and right with arrows

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    // 360 is the maximum left position value (400 (game width) - 40 (dodger width))
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
